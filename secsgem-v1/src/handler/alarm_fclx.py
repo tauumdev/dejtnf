e1712a29656691c2adf8358a00c30c0bc3893edb
tauum <tauum.dev@gmail.com>
2025-01-16 13:04:21 +0700

    init upload

--- secsgem-v1/src/handler/alarm_fclx.py
+++ secsgem-v1/src/handler/alarm_fclx.py
@@ -1,4 +1,3 @@
-
 import json
 import logging
 from typing import TYPE_CHECKING
@@ -10,12 +9,14 @@ logger = logging.getLogger("app_logger")
 
 
 class HandlerAlarmFCLX:
-    def __init__(self, equipment):
+    def __init__(self, equipment: 'Equipment'):
         self.equipment = equipment
 
     def handle_alarm_fclx(self, ppname, lot_id, alid, alcd, altx):
         try:
-            msg = json.dumps({"ALID": alid, "ALCD": alcd, "ALTX": altx})
+            msg = json.dumps({"EquipmentName": self.equipment.equipment_name,
+                              "PPName": ppname, "LotID": lot_id,
+                              "ALID": alid, "ALCD": alcd, "ALTX": altx})
             logger.info("%s Alarm: %s", self.equipment.equipment_name, msg)
             self.equipment.mqtt_client.client.publish(
                 f"equipments/status/alarm/{self.equipment.equipment_name}", msg, qos=1, retain=True)
