9cd6b346a51f69497974ebbec8163a80e127cac5
tauum <tauum.dev@gmail.com>
2025-02-04 16:56:12 +0700

    initial upload

--- secsgem/main.py
+++ secsgem/main.py
@@ -1,19 +1,18 @@
 import logging
-from src.cli.command_cli import Cli
-from src.equipment_manager.eq_manager import EquipmentManager
-from src.mqtt.client.mqtt_client import MqttClient
+
+from src.cli.main_cli import CommandCli
+from src.mqtt.mqtt_client import MqttClient
 from src.utils.logger.app_logger import AppLogger
+
 
 app_logger = AppLogger()
 logger = app_logger.get_logger()
 logger.setLevel(logging.INFO)
 
-if __name__ == "__main__":
 
-    logger.info("Starting program...")
+if __name__ == "__main__":
+    logger.info("Starting secs/gem dejtnf")
 
     mqtt_client = MqttClient()
-    eq_manager = EquipmentManager(mqtt_client)
-
-    cmd = Cli(eq_manager)
-    cmd.cmdloop()
+    cli = CommandCli(mqtt_client)
+    cli.cmdloop()
